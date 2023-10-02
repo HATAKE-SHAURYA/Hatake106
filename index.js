4e3bddbf5c1a3fa7d7adfa041044dd3c17162085
HATAKE-SHAURYA <146454683+HATAKE-SHAURYA@users.noreply.github.com>
2023-10-02 19:42:06 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+burger = document.querySelector('.burger')
+header = document.querySelector('.header')
+navList = document.querySelector('.nav-list')
+burger.addEventListener('click',()=>{
+  header.classList.toggle('.h-nav')
+  navList.classList.toggle('.v-class')
+})
