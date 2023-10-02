c4acc0f5a827cf058a1b43764cb1a2cd42c726b8
HATAKE-SHAURYA <146454683+HATAKE-SHAURYA@users.noreply.github.com>
2023-10-02 19:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ burger = document.querySelector('.burger')
 header = document.querySelector('.header')
 navList = document.querySelector('.nav-list')
 burger.addEventListener('click',()=>{
-  header.classList.toggle('.h-nav')
-  navList.classList.toggle('.v-class')
+  header.classList.toggle('.h-nav');
+  navList.classList.toggle('.v-class');
 })
